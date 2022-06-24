df2a338a169f00ce4bb85f54768b6c9971a1595b
lucas patrick <lucas.patrick.lsilva@gmail.com>
2022-06-24 20:31:25 -0300

    placeholder

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -10,7 +10,13 @@ export const Event = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        {slug ? <Video lessonSlug={slug} /> : <div className="flex-1"></div>}
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1">
+            <p className="text-blue-500 text-2xl text-center my-40">Clique em algum vídeo para começar</p>
+          </div>
+        )}
         <Sidebar />
       </main>
     </div>
